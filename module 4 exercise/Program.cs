1eb687e9505fdedb8e4a6c8a6ac32f61b0cb4e72
Kostya <113539367+KonstantinValkovsky@users.noreply.github.com>
2023-05-01 22:05:17 +0300

    закончил итоговое задание модуля 4

--- module 4 exercise/Program.cs
+++ module 4 exercise/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace module_4_exercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace module_4_exercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            (string Name, string Surname, string Login, int CountLogin, bool HasPet, double Age, string[] favcolors ) Anketa;
+
+            for (int k = 0; k < 3; k++)
+            {
+                Console.Write("Введите имя: ");
+                Anketa.Name = Console.ReadLine();
+
+                Console.Write("Введите фамилию :");
+                Anketa.Surname = Console.ReadLine();
+
+                Console.Write("Ввведите логин: ");
+                Anketa.Login = Console.ReadLine();
+                Anketa.CountLogin = Anketa.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животное? ДА или Нет");
+                var result = Console.ReadLine();
+
+                if (result == "Да")
+                {
+                    Anketa.HasPet = true;
+                }
+                else
+                {
+                    Anketa.HasPet = false;
+                }
+
+                Console.Write("Введите возраст пользователя: ");
+                Anketa.Age = double.Parse(Console.ReadLine());
+
+                Anketa.favcolors = new string[3];
+                Console.WriteLine("Введите три своих любимых цвета: ");
+                for (int i = 0; i < Anketa.favcolors.Length; i++)
+                {
+                    Anketa.favcolors[i] = Console.ReadLine();
+                }
+            }
+
+
+            Console.ReadKey();
         }
+
     }
 }
